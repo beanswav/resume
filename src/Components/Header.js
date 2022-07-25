bba2a5c276ecf98d7ee884b4bddf9b71bc73b551
Vince <vinceestrada1215@gmail.com>
2022-07-24 22:44:48 -0500

    no works tab

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -29,11 +29,6 @@ export default function Header(props) {
             </a>
           </li>
           <li>
-            <a className="smoothscroll" href="#portfolio">
-              Works
-            </a>
-          </li>
-          <li>
             <a className="smoothscroll" href="#testimonials">
               Testimonials
             </a>
